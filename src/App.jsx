1fe180cf9ca8a21f5d8a881cdc64afe6abc199d7
Andy Learmouth <andymlearmouth@hotmail.com>
2023-10-17 18:13:35 +0100

    Added conditional messaging on the results page.

--- src/App.jsx
+++ src/App.jsx
@@ -162,6 +162,16 @@ function AnswerButton({ answer, setQuestionNumber, setNumCorrectAnswers }) {
 }
 
 function Result({ numCorrectAnswers, quiz }) {
+
+  let resultMessage = ''
+  if(numCorrectAnswers <= Math.floor(quiz.length/2)){
+    resultMessage = 'A bit more practice required, you scored'
+  }else if(numCorrectAnswers > Math.floor(quiz.length/2) && numCorrectAnswers <= Math.floor(quiz.length/1.5)){
+    resultMessage = 'Well done you scored'
+  }else{
+    resultMessage = 'Well done you scored an excellent'
+  }
+
   function handleClick(){
     window.location.reload();
 }
@@ -170,7 +180,7 @@ function Result({ numCorrectAnswers, quiz }) {
     <div className="card lg:card-side bg-base-100 shadow-xl h-80 flex bg-slate-600">
       <div className="card-body flex">
         <h1 className="text-5xl mb-8">
-          Well done you scored {numCorrectAnswers} / {quiz.length}!
+          {resultMessage} {numCorrectAnswers} / {quiz.length}!
         </h1>
         <div>
           <button className="btn btn-neutral btn-primary m-2 btn-answer w-11/12 h-14 text-xl"
